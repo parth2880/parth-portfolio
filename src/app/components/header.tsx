3a08aa79a1640e5282948023729d38c00797a180
Parth <parthsharma2880@gmail.com>
2025-03-23 15:47:53 +0530

    Initial commit

---
+++ src/app/components/header.tsx
@@ -0,0 +1,55 @@
+// components/Header.tsx
+'use client'; // Add this line to indicate this is a client component
+
+import React, { useState } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation'; // Use this instead of useRouter
+
+type NavItem = {
+  label: string;
+  href: string;
+};
+
+const Header: React.FC = () => {
+  const pathname = usePathname(); // Use pathname instead of router
+  
+  const navItems: NavItem[] = [
+    { label: 'home', href: '/' },
+    { label: 'works', href: '/works' },
+    { label: 'about-me', href: '/about-me' },
+    { label: 'contacts', href: '/contacts' },
+  ];
+  
+  const toggleLanguage = () => {
+    setLanguage(language === 'EN' ? 'FR' : 'EN');
+  };
+  
+  return (
+    <header className="w-full py-4 px-6">
+      <div className="max-w-7xl mx-auto flex justify-between items-center">
+        <div className="flex items-center space-x-2">
+          <Image src="/icons/site-logo.svg" alt="logo" width={16} height={16} />
+          <span className="font-semibold">Parth</span>
+        </div>
+        
+        <nav className="flex items-center space-x-8">
+          {navItems.map((item) => (
+            <Link 
+              href={item.href} 
+              key={item.href}
+              className={`relative ${
+                pathname === item.href ? 'text-purple-400' : 'text-gray-300 hover:text-white'
+              }`}
+            >
+              <span className="mr-1">#</span>
+              {item.label}
+            </Link>
+          ))}
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <Image src="/icons/structure1.svg" alt="structure" width={113} height={113} />
+    </div>
+  );
+}
