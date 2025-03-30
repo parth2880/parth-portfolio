3ec7830214c91140bd2b5812241a5871a3825b80
Parth <parthsharma2880@gmail.com>
2025-03-30 17:14:25 +0530

    Added: skills and aboutme section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Hero from "./components/Hero";
 import Title from "./components/Title";
 import Card from "./components/Card";
+import Button from "./components/Button";
 
 export default function Home() {
   return (
@@ -35,12 +36,110 @@ export default function Home() {
         <Card body={<>Minecraft servers hosting </>} />
         <Card body={<>Minecraft servers hosting </>} />
       </div>
-      <Title>skills</Title>
 
+      <Title className="mb-12">skills</Title>
+      <div className="flex justify-between gap-8">
+        <div className="relative w-[35%]">
+          <Image
+            src="/icons/dots.svg"
+            alt=""
+            width={64}
+            height={64}
+            className="absolute top-0 left-0"
+          />
+          <Image
+            src="/icons/dots.svg"
+            alt=""
+            width={64}
+            height={64}
+            className="absolute top-1/2 right-0"
+          />
+          <Image
+            src="/icons/structure1.svg"
+            alt=""
+            width={113}
+            height={113}
+            className="absolute bottom-0 left-8"
+          />
+          <div className="border size-22 absolute -top-8 right-8 border-secondary"></div>
+          <div className="border size-16 absolute bottom-8 right-0 border-secondary"></div>
+        </div>
+        <div className="flex gap-4">
+          <div className="flex flex-col gap-4">
+            <Card className="inline-block flex-col max-w-44">
+              <p className="p-2 border-b">Languages</p>
+              <div className="text-secondary p-2 flex flex-wrap gap-2">
+                <p>Typescript</p>
+                <p>Typescript</p>
+              </div>
+            </Card>
+          </div>
 
-      <Title>about-me</Title>
-      
-      
+          <div className="flex flex-col gap-4">
+            <Card className="inline-block flex-col max-w-44">
+              <p className="p-2 border-b">Languages</p>
+              <div className="text-secondary p-2 flex flex-wrap gap-2">
+                <p>Typescript</p>
+                <p>Typescript</p>
+              </div>
+            </Card>
+            <Card className="inline-block flex-col max-w-44">
+              <p className="p-2 border-b">Languages</p>
+              <div className="text-secondary p-2 flex flex-wrap gap-2">
+                <p>Typescript</p>
+                <p>Typescript</p>
+              </div>
+            </Card>
+          </div>
+
+          <div className="flex flex-col gap-4">
+            <Card className="inline-block flex-col max-w-44">
+              <p className="p-2 border-b">Languages</p>
+              <div className="text-secondary p-2 flex flex-wrap gap-2">
+                <p>Typescript</p>
+                <p>Typescript</p>
+              </div>
+            </Card>
+            <Card className="inline-block flex-col max-w-44">
+              <p className="p-2 border-b">Languages</p>
+              <div className="text-secondary p-2 flex flex-wrap gap-2">
+                <p>Typescript</p>
+                <p>Typescript</p>
+              </div>
+            </Card>
+            <Card className="inline-block flex-col max-w-44">
+              <p className="p-2 border-b">Languages</p>
+              <div className="text-secondary p-2 flex flex-wrap gap-2">
+                <p>Typescript</p>
+                <p>Typescript</p>
+              </div>
+            </Card>
+          </div>
+        </div>
+      </div>
+
+      <Title className="mt-28">about-me</Title>
+      <div className="flex justify-between gap-4 *:w-1/2 mt-6">
+        <div className="text-secondary space-y-8">
+          <p>Hello, i’m Elias! </p>
+          <p>
+            I’m a self-taught front-end developer based in Kyiv, Ukraine. I can
+            develop responsive websites from scratch and raise them into modern
+            user-friendly web experiences.
+          </p>
+          <p>
+            Transforming my creativity and knowledge into a websites has been my
+            passion for over a year. I have been helping various clients to
+            establish their presence online. I always strive to learn about the
+            newest technologies and frameworks.
+          </p>
+          <Button>Read more !!</Button>
+        </div>
+        <div>
+          <Image src="/images/parth-hero.png" alt="" width={457} height={386} className="ml-auto" />
+        </div>
+      </div>
+
       <Title>contacts</Title>
     </>
   );
